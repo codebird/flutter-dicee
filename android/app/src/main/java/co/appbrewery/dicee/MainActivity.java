b122fa9bc148fd53d313d53ec0f768dd0f7509b1
Hicham Mallah <mallah.hicham@gmail.com>
2021-05-11 13:38:31 +0300

    initial commit

--- android/app/src/main/java/co/appbrewery/dicee/MainActivity.java
+++ android/app/src/main/java/co/appbrewery/dicee/MainActivity.java
@@ -1,4 +1,4 @@
-package co.appbrewery.dicee;
+package com.hmallah.dicee;
 
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -9,15 +10,56 @@ void main() {
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: Dicee(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class Dicee extends StatefulWidget {
+  const Dicee({Key key}) : super(key: key);
+
+  @override
+  _DiceeState createState() => _DiceeState();
+}
+
+class _DiceeState extends State<Dicee> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(5) + 1;
+      rightDiceNumber = Random().nextInt(5) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      alignment: Alignment.center,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
